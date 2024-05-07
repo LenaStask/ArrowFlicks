9eb3617014fdc5aca70dda23b47c36315dc29eab
lenaStsk <staskevichalena@gmail.com>
2024-05-07 22:47:13 +0200

    Add component Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Flex } from "@mantine/core";
+import { Logo } from "../Logo/Logo";
+import classes from "./Navbar.module.css";
+import { useState } from "react";
+
+const data = [
+  { link: "", label: "Movies" },
+  { link: "", label: "Rated movies" },
+];
+
+const Navbar = () => {
+  const [active, setActive] = useState("Movies");
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <span>{item.label}</span>
+    </a>
+  ));
+  return (
+    <Flex className={classes.navbar} bg="purple.5">
+      <Logo />
+      <Flex className={classes.navigation}>{links}</Flex>
+    </Flex>
+  );
+};
+
+export { Navbar };
