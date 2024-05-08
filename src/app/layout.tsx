1f04f05b865e9c64f41037631131a2967d501129
lenaStsk <staskevichalena@gmail.com>
2024-05-08 13:50:23 +0200

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { MantineProvider } from '@mantine/core';
-import { theme } from '../../theme';
-import { Logo } from '@/components/Logo/Logo';
+import { Flex, MantineProvider } from "@mantine/core";
+import { theme } from "../../theme";
+import { Navbar } from "@/components/Navbar/Navbar";
+import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,14 +16,19 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </head>
       <body className={inter.className}>
-        <MantineProvider theme={theme}><Logo/>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <Flex className="container">
+            <Navbar />
+            <main>{children}</main>
+          </Flex>
+        </MantineProvider>
       </body>
     </html>
   );
