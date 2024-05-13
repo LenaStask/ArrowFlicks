0a1914cd03959f56d5466ad30a0e83345bd7faba
lenaStsk <staskevichalena@gmail.com>
2024-05-13 11:49:06 +0200

    Update Logo component styles

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,20 +1,27 @@
-import NextImage from 'next/image';
-import { Box, Center, Image, Text } from '@mantine/core';
-import icon from '../../../public/icon.svg';
+import NextImage from "next/image";
+import { Box, Center, Image, Text, Title } from "@mantine/core";
+import icon from "../../../public/icon.svg";
 import { Poppins } from "next/font/google";
-import classes from './Logo.module.css';
+import classes from "./Logo.module.css";
 
-const poppins = Poppins({subsets: ['latin'], weight: ['600']});
+const poppins = Poppins({ subsets: ["latin"], weight: ["600"] });
 
 const Logo = () => {
   return (
-    <Box  className={classes.logo} component='a' href='/'>
+    <Box className={classes.logo} component="a" href="/">
       <Center className={classes.center}>
-        <Image className={classes.image} component={NextImage} src={icon} alt='icon'/>
-        <Text className={poppins.className}  span c='purple.0' >ArrowFlicks</Text>
+        <Image
+          className={classes.image}
+          component={NextImage}
+          src={icon}
+          alt="icon"
+        />
+        <Title className={poppins.className} c="purple.1">
+          ArrowFlicks
+        </Title>
       </Center>
     </Box>
   );
-}
+};
 
-export {Logo};
+export { Logo };
