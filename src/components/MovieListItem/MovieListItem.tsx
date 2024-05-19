926acdcc87b7dcbf33eb5ef8af8625dbc2740bb0
lenaStsk <staskevichalena@gmail.com>
2024-05-19 21:35:30 +0200

    Update MovieListItem component

--- src/components/MovieListItem/MovieListItem.tsx
+++ src/components/MovieListItem/MovieListItem.tsx
@@ -1,18 +1,30 @@
 import NextImage from "next/image";
-import { Card, Image, Text, Group, Flex } from "@mantine/core";
+import { Card, Image, Text, Group, Flex, Title } from "@mantine/core";
 import classes from "./MovieListItem.module.css";
 import { IconStarFilled } from "@tabler/icons-react";
-import IMovie from "@/app/interfaces/IMovie";
 import { useQuery } from "@tanstack/react-query";
-import { getGenresList } from "@/app/api/discover";
+import { getGenresList } from "@/api/tmdb";
+import no_poster from "../../assets/no_poster.svg";
+import IMovie from '@/app/interfaces/IMovie';
 
 const MovieListItem = ({ movie }: { movie: IMovie }) => {
   const { data, isSuccess } = useQuery({
-    queryFn: getGenresList,
+    queryFn: () => getGenresList(),
     queryKey: ["genres"],
   });
+
   return (
-    <Card className={classes.card}>
+    <Card className={classes.card} component="a" href={`/movie/${movie.id}`}>
+      {movie.poster_path === null ? (
+        <Image
+          priority={true}
+          src={no_poster}
+          width={119}
+          height={170}
+          component={NextImage}
+          alt="poster"
+        />
+      ) : (
         <Image
           priority={true}
           src={"https://image.tmdb.org/t/p/w185" + movie.poster_path}
@@ -21,23 +33,32 @@ const MovieListItem = ({ movie }: { movie: IMovie }) => {
           component={NextImage}
           alt="poster"
         />
+      )}
       <Flex className={classes.info}>
         <Flex direction="column">
-          <Text className={classes.title}>{movie.title}</Text>
+          <Title className={classes.title} order={2}>{movie.title}</Title>
           <Text className={classes.year}>{movie.release_date.slice(0, 4)}</Text>
           <Group className={classes.vote}>
             <IconStarFilled color="#FAB005" />
-            <Text className={classes.average}>{movie.vote_average.toFixed(1)}</Text>
+            <Text className={classes.average}>
+              {movie.vote_average.toFixed(1)}
+            </Text>
             <Text className={classes.count}>({movie.vote_count})</Text>
           </Group>
         </Flex>
         <Text className={classes.genres}>
-          Genres <span>{isSuccess && data.reduce((res:string[], item) => {
-            if(movie.genre_ids.includes(item.id)) {
-              res.push(item.name)
-            }
-            return res;
-          } ,[]).join(', ')}</span>
+          Genres{" "}
+          <span>
+            {isSuccess &&
+              data.genres
+                .reduce((res: string[], item) => {
+                  if (movie.genre_ids.includes(item.id)) {
+                    res.push(item.name);
+                  }
+                  return res;
+                }, [])
+                .join(", ")}
+          </span>
         </Text>
       </Flex>
       <Flex className={classes.iconStar}>
