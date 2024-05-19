d30c170e5941d1d1f87fda21a98d0c8fdf665526
lenaStsk <staskevichalena@gmail.com>
2024-05-19 21:36:12 +0200

    Update MovieList component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,17 +1,13 @@
-import { Container, Flex, Group } from '@mantine/core';
+import { Container } from "@mantine/core";
 import classes from "./MovieList.module.css";
-import MovieListItem from '../MovieListItem/MovieListItem';
-import IMovie from '@/app/interfaces/IMovie';
+import MovieListItem from "../MovieListItem/MovieListItem";
+import IMovie from "@/interfaces/IMovieListItem";
 
-const MovieList = ({movies}:{movies: IMovie[]}) => {
-  const children = movies.map((movie): React.ReactNode => 
-    <MovieListItem key={movie.id} movie={movie}/>
-  )
-  return (
-    <Container className={classes.container}>
-      {children}
-    </Container>
+const MovieList = ({ movies }: { movies: IMovie[] }) => {
+  const children = movies.map(
+    (movie): React.ReactNode => <MovieListItem key={movie.id} movie={movie} />
   );
-}
+  return <Container className={classes.container}>{children}</Container>;
+};
 
 export default MovieList;
